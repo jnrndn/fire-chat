d4e58183960b3d18bdc6ce1c0b4f0e8755f55132
Unknown <juan.rendon@yuxiglobal.com>
2018-09-03 17:43:08 -0500

    Feat: firebase config

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCSSUcpe5Wx8-wcYwu-6A_j2X4jr3gACX8',
+  authDomain: 'fire-chat-48c6d.firebaseapp.com',
+  databaseURL: 'https://fire-chat-48c6d.firebaseio.com',
+  projectId: 'fire-chat-48c6d',
+  storageBucket: 'fire-chat-48c6d.appspot.com',
+  messagingSenderId: '466740404992',
+};
