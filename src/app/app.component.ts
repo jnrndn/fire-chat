1214f6bed0f13f775b3034255429da1855578ad1
Unknown <juan.rendon@yuxiglobal.com>
2018-09-03 17:42:44 -0500

    Feat: Login using google

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
-
+import { AngularFireAuth } from 'angularfire2/auth';
+import { auth } from 'firebase/app';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.scss' ],
 })
 export class AppComponent {
-  title = 'fire-chat';
+
+  constructor(public afAuth: AngularFireAuth) { }
+
+  googleLogin() {
+    this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider);
+  }
+
+  githubLogin() {
+    this.afAuth.auth.signInWithPopup(new auth.GithubAuthProvider);
+  }
+
+  logout() {
+    this.afAuth.auth.signOut();
+  }
 }
