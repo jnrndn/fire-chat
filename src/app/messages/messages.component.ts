d6c1de2439a90be3aae33e90def717a4cbc897ee
Unknown <juan.rendon@yuxiglobal.com>
2018-09-03 21:01:30 -0500

    Fix: use relative path to deploy to gh-pages

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Message } from '../message.interface';
+import { Message } from './../message.interface';
 
 @Component({
   selector: 'app-messages',
